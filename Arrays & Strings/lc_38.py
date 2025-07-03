baadec5b5971530518d206ea8ea0696450bfaf05
Moayyad Shahid <moayyad.shahid1@gmail.com>
2025-07-02 23:49:24 -0400

    annotations

--- Arrays & Strings/lc_38.py
+++ Arrays & Strings/lc_38.py
@@ -3,28 +3,32 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+
         n = len(matrix)
+        #The strategy here is that we will first take the Transpose, and then Horizontal Reflection
+
         # Transpose
+        # Go through each row
         for i in range(0, n):
-            for j in range(i+1, n):
+            # We will see each element that starts one more from the diagonal
+            # so here we choose column index j, that is 1 more than the current diagonal element
+            for j in range(i + 1, n):
+                # swap the elements across the diagonal, so ij <-> ji
                 temp = matrix[i][j]
                 matrix[i][j] = matrix[j][i]
                 matrix[j][i] = temp
         
         # Horizontal Flip
-        if n % 2 == 0:
-            for i in range(0, n):
-                for j in range(0, n // 2):
-                    temp = matrix[i][j]
-                    matrix[i][j] = matrix[i][n - j - 1]
-                    matrix[i][n - j - 1] = temp
-        else:
-            for i in range(0, n):
-                for j in range(0, n // 2):
-                    temp = matrix[i][j]
-                    matrix[i][j] = matrix[i][n - j - 1]
-                    matrix[i][n - j - 1] = temp
-            
+        # go through each row
+        for i in range(0, n):
+            # go to the mid-way point of each row
+            for j in range(0, n // 2):
+                # swap the elements with its corresponding outer-pair
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[i][n - j - 1]
+                matrix[i][n - j - 1] = temp
+
+        # Time Complexity: O(N^2), since we do 2 nested loops of size N
 
 
         
