4e9acaeb272371daa8e4f54dfc949b59e3ca1674
Moayyad Shahid <moayyad.shahid1@gmail.com>
2025-07-04 23:54:34 -0400

    restart solution

--- Arrays & Strings/lc_56.py
+++ Arrays & Strings/lc_56.py
@@ -1,11 +1,3 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        merged = []
-        for i in range(len(intervals) - 1):
-            interval = intervals[i]
-            if interval[-1] >= intervals[i+1][0]:
-                new_interval = [interval[0], intervals[i+1][0]]
-                merged.append(new_interval)
-
-        merged.append(intervals[-1])
-        return merged
+        
