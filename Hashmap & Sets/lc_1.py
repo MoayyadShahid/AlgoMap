eedc9904a25078a0b943c4b8495e2f1883d9ce9f
Moayyad Shahid <moayyad.shahid1@gmail.com>
2025-08-28 11:29:01 +0400

    O(N) Solution

--- Hashmap & Sets/lc_1.py
+++ Hashmap & Sets/lc_1.py
@@ -1,14 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # create empty the list of 2 index
-        rng = []
-        # go through each element index
-        for m in range(len(nums)):
-            # loop over every element index above the current one (m)
-            for n in range(m + 1, len(nums)):
-                # check it the sum of the current m and n equate to target
-                if nums[m] + nums[n] == target:
-                    rng = [m, n]
-        return rng
-        # Run Time: O(N^2)
+        # create a hashmap to store the element:index key-value pairs
+        sMap = {}
+        # loop through the nums list and add the elements to the hashmap, and update their index if 
+        # an element re-occurs later on in the list
+        for i in range(len(nums)):
+            sMap[nums[i]] = i
+        
+        # loop through the nums list again and this time we will see if the complement integer exists in the hashmap
+        for i in range(len(nums)):
+            # define the complement integer as 'need' for the current integer nums[i]
+            need = target - nums[i]
+            # we see if 'need' is inside the hashmap, and also that it is at a different index than i
+            # if so, then we have found our pair
+            if need in sMap and sMap[need] != i:
+                return [i, sMap[need]]
+        # Run Time: O(N)
         
