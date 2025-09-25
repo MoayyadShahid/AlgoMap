d88ffce6ceffb6ec3dd36651a67e26ba5dbfc0d5
Moayyad Shahid <moayyad.shahid1@gmail.com>
2025-09-24 23:20:21 -0400

    O(1) Solution

---
+++ Stacks & Queues/lc_155.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.stk = []
+        self.min_stk = []
+
+    def push(self, val: int) -> None:
+        self.stk.append(val)
+        if self.min_stk == []:
+            self.min_stk.append(val)
+        elif val < self.min_stk[-1]:
+            self.min_stk.append(val)
+        else:
+            self.min_stk.append(self.min_stk[-1])
+
+    def pop(self) -> None:
+        if self.stk != []:
+            self.stk.pop()
+            self.min_stk.pop()
+        
+
+    def top(self) -> int:
+        return self.stk[-1]
+
+    def getMin(self) -> int:
+        return self.min_stk[-1]
+    
