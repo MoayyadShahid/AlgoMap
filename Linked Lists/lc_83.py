1b9207457be8822981ee0c0a5e45dda073e8a458
Moayyad Shahid <moayyad.shahid1@gmail.com>
2025-09-25 22:46:27 -0400

    O(N) Solution

---
+++ Linked Lists/lc_83.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        final = head
+        curr = head
+        while curr:
+            if curr.next != None and curr.next.val == curr.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return final
+        # Run Time: O(N)
+        
