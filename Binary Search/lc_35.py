4679f80ada66870a41de8a8ded7f10b9dba957db
Moayyad Shahid <moayyad.shahid1@gmail.com>
2025-10-13 21:31:33 -0400

    Add Annotations

--- Binary Search/lc_35.py
+++ Binary Search/lc_35.py
@@ -1,12 +1,25 @@
 class Solution:
+    '''
+    this is a slight modification of the classic binary search algorithm. We will check 5 things when
+    we will calculate the mid and use it in the following way:
+
+    1. if the target is equal to the num at mid index we return mid index 
+    2. otherwise if the target is greater than the highest element we return highest index + 1
+    3. otherwise if the target is smaller than the lowest element we return lowest index (since lowest would become lowest + 1)
+    4. otherwise if the target is smaller than the mid index then we update the high index to be smaller
+    5. otherwise if the arget is greater than the mid index then we update the low index to be higher
+    '''
     def searchInsert(self, nums: List[int], target: int) -> int:
-        
+        # set the 2 ptrs for low and high index
         low = 0
         high = len(nums) - 1
 
+        # loop through the array until we have a crossover of low and high index where low > high
         while low <= high:
+            # we calculate the mid index
             mid = (low + high) // 2
 
+            # go through the conditions we outlined above
             if target == nums[mid]:
                 return mid
             elif target > nums[high]:
@@ -18,3 +31,4 @@ class Solution:
             elif nums[mid] < target:
                 low = mid + 1            
      
+    # Run Time: O(log N)
--- Binary Search/lc_704.py
+++ Binary Search/lc_704.py
@@ -1,16 +1,27 @@
 class Solution:
+    '''
+    This is a classic binary search algorithm implemenation
+    '''
     def search(self, nums: List[int], target: int) -> int:
+        # we create a low and high, classic 2 ptr technique
         low = 0
         high = len(nums) - 1
 
+        # we loop through the array, until we get a crossover of low and high, where low > high
         while low <= high:
+            # we calculate the mid point
             mid = (low + high) // 2
 
+            # we check if the num at mid is equal, then return the mid index
             if nums[mid] == target:
                 return mid
+            # otherwise if mid index num is greater than target, we adjust the high index to be lower
             elif nums[mid] > target:
                 high = mid - 1
+            # else if the mid index num is lesser than target, we adjust the lwo index to be higher
             else:
                 low = mid + 1
         
+        # if we didn't return a valid index, we return -1 to indicate failure to find
         return -1
+    # Run Time: O(log N)
