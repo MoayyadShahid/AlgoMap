124fd9933d6ac678a2a4ab89b75a1c941c5b7b1e
Moayyad Shahid <moayyad.shahid1@gmail.com>
2026-04-24 19:33:09 -0400

    O(N * N!) Solution

--- Recursive Backtracking/lc_46.py
+++ Recursive Backtracking/lc_46.py
@@ -1,21 +1,32 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        # the idea behind permutations is that we have different orderings of a list of numbers
+        # the way we do this is by seeing which numbers we have already used and only recursing on the remaining numbers
         n = len(nums)
         res =[]
         sol = []
         used = set()
 
         def backtrack(i):
+            # the depth of our recursion will be the size of the permutation we can generate which is size n
             if i == n:
                 res.append(sol[:])
+                return
             
+            # at each recursion we will go through all the possible numbers in nums, from index 0 to n - 1
             for j in range(n):
+                # we must check that the current number we are considering is not already used in our permutation
                 if nums[j] not in used:
+                    # we add that new number
                     sol.append(nums[j])
+                    # we also track that new number as used
                     used.add(nums[j])
+                    # now we go onto the next index of our permutation
                     backtrack(i + 1)
                     sol.pop()
                     used.remove(nums[j])
             
+        # begin the backtracking
         backtrack(0)
         return res
+    # Run Time: O(N * N!)
